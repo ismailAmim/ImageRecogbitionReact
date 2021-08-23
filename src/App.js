4b9879e689d0b675f37f1d934ae249a0916412d7
ismailAmim <ismail.amim2010@gmail.com>
2021-08-23 18:09:55 +0100

    first installation

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      {
+        /*
+      <Logo/>
+      <InputLink/>
+      <ImageRecognition/>  */}  </div>
+  );
+}
+
+export default App;
